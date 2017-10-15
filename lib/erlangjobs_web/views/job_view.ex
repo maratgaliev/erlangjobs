78cad9ebd1950d70a75a1f2870292b792ba06595
Petr Stepchenko <nitrinodev@gmail.com>
2017-10-15 17:37:39 +0300

    update in accordance with ideology of phoenix 1.3

--- lib/erlangjobs_web/views/job_view.ex
+++ lib/erlangjobs_web/views/job_view.ex
@@ -5,10 +5,18 @@ defmodule ErlangjobsWeb.JobView do
   def markdown(description) do
     description
     |> Earmark.as_html!
-    |> raw
   end
 
   def translate_options(map) do
     for {k, v} <- Map.new(map), into: %{}, do: {Gettext.gettext(ErlangjobsWeb.Gettext, String.capitalize(Atom.to_string(k))), v}
+  end
+
+
+  def job_link(id), do: "#{ErlangjobsWeb.Endpoint.url}/#job_#{id}"
+
+  def date_format_rfc1123(inserted_at) do
+    inserted_at
+      |> Timex.to_datetime
+      |> Timex.format!("{RFC1123}")
   end
 end
