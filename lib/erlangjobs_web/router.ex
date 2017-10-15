4a42d4851f1b4ed97fdaf155815b6b21b5020465
Marat Galiev <kazanlug@gmail.com>
2017-10-15 22:18:06 +0300

    Add separate job page

--- lib/erlangjobs_web/router.ex
+++ lib/erlangjobs_web/router.ex
@@ -15,7 +15,7 @@ defmodule ErlangjobsWeb.Router do
 
   scope "/", ErlangjobsWeb do
     pipe_through :browser # Use the default browser stack
-    resources "/jobs", JobController, only: [:index, :new, :create]
+    resources "/jobs", JobController, only: [:index, :new, :create, :show]
 
     get "/", JobController, :index
     get "/feed", JobController, :feed
