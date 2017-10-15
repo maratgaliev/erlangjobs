ebf17efe68dab29f49ff243e428ac51e377b8813
Marat Galiev <kazanlug@gmail.com>
2017-10-16 01:13:18 +0300

    Travis CI addons

---
+++ config/travis.exs
@@ -0,0 +1,26 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :erlangjobs, ErlangjobsWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :erlangjobs, Erlangjobs.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "erlangjobs_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+config :erlangjobs, Erlangjobs.Twitter,
+  client: Erlangjobs.Twitter.StubClient
+
+if File.exists?("config/test.local.exs") do
+  import_config "test.local.exs"
+end
