e54f9ec2cd6810798a974793a9817cbfc33c909f
Marat Galiev <kazanlug@gmail.com>
2017-10-15 22:18:06 +0300

    Add separate job page

--- lib/erlangjobs_web/views/job_view.ex
+++ lib/erlangjobs_web/views/job_view.ex
@@ -11,7 +11,6 @@ defmodule ErlangjobsWeb.JobView do
     for {k, v} <- Map.new(map), into: %{}, do: {Gettext.gettext(ErlangjobsWeb.Gettext, String.capitalize(Atom.to_string(k))), v}
   end
 
-
   def job_link(id), do: "#{ErlangjobsWeb.Endpoint.url}/#job_#{id}"
 
   def date_format_rfc1123(inserted_at) do
