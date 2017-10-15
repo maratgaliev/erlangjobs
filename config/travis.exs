a9439127cf444db274905848b9d6730e06f7ec49
Marat Galiev <kazanlug@gmail.com>
2017-10-16 01:22:20 +0300

    Fix typo in CI config

--- config/travis.exs
+++ config/travis.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :erlangjobs, Erlangjobs.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "erlangjobs_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
