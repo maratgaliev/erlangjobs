7964f8aca311f90f5ff3c2a3e6a55928296f42e7
Marat Galiev <kazanlug@gmail.com>
2017-10-16 00:57:54 +0300

    Add twitter env variables, travis fixes

--- config/prod.exs
+++ config/prod.exs
@@ -23,6 +23,12 @@ config :erlangjobs, ErlangjobsWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :extwitter, :oauth, [
+   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+   access_token_secret: System.get_env("TWITTER_ACCESS_TOKEN_SECRET")
+]
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
