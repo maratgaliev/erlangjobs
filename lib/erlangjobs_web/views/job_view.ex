bf27f11d7731bb98f95732fdf2797e4dab3874c9
Marat Galiev <kazanlug@gmail.com>
2017-10-16 16:45:02 +0300

    Temp fix link helper

--- lib/erlangjobs_web/views/job_view.ex
+++ lib/erlangjobs_web/views/job_view.ex
@@ -11,7 +11,8 @@ defmodule ErlangjobsWeb.JobView do
     for {k, v} <- Map.new(map), into: %{}, do: {Gettext.gettext(ErlangjobsWeb.Gettext, String.capitalize(Atom.to_string(k))), v}
   end
 
-  def job_link(id), do: "#{ErlangjobsWeb.Endpoint.url}/#job_#{id}"
+  #TODO
+  def job_link(id), do: "#{ErlangjobsWeb.Endpoint.url}/jobs/{id}"
 
   def date_format_rfc1123(inserted_at) do
     inserted_at
