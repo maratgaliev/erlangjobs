972aca6f87829bf7e8c3051185d345dbf8e37fb8
Petr Stepchenko <nitrinodev@gmail.com>
2017-10-18 00:49:43 +0300

    fix job links in rss feed

--- lib/erlangjobs_web/views/job_view.ex
+++ lib/erlangjobs_web/views/job_view.ex
@@ -11,9 +11,6 @@ defmodule ErlangjobsWeb.JobView do
     for {k, v} <- Map.new(map), into: %{}, do: {Gettext.gettext(ErlangjobsWeb.Gettext, String.capitalize(Atom.to_string(k))), v}
   end
 
-  #TODO
-  def job_link(id), do: "#{ErlangjobsWeb.Endpoint.url}/jobs/#{id}"
-
   def date_format_rfc1123(inserted_at) do
     inserted_at
       |> Timex.to_datetime
